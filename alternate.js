be52e20fac246264ad1b496c896f5051f556f114
Andrew Hase <oldm8reggie@gmail.com>
2018-06-11 11:17:26 +1000

    Comments, spelling and cleaning

--- alternate.js
+++ alternate.js
@@ -1,18 +1,19 @@
 //Small Icon Rotator for personal use
+// List the class names of all icons you want use here. 
 var iconList = ["fab fa-js fa-7x", "fab fa-php fa-7x", "fab fa-html5 fa-7x", "fas fa-code-branch fa-7x", "fas fa-user-astronaut fa-7x"];
-var devIcon = document.getElementById('devIcons');
+var devIcon = document.getElementById('devIcons'); //ID of FA icon you want to alternate
 devIcon.className = iconList[0];
 var listL = iconList.length;
 /*
+FA Icons I have used
  0 = JS 
- 1=php
- 2=html
- 3=git
- 4 = astronaught
+ 1 = php
+ 2 = html
+ 3 = git
+ 4 = astronaut
 */
 var count = 0
 var iconRotator = setInterval(function () {
-    console.log(iconList[(++count) % listL]);
     devIcon.className = iconList[(++count) % listL];
 }, 3000);
 
